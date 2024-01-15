f9c2c1849f4989a3979cee2e4ba5a221922fcc11
Pavel Abydennov <pabydennov@aq.ru>
2024-01-15 14:57:32 +0300

    Disable some language providers

--- init.lua
+++ init.lua
@@ -37,6 +37,12 @@ vim.keymap.set('n', ' ', '', {})
 vim.g.mapleader = ' '
 
 
+-- Disable language providers
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_node_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 
 -- Setup default assembler syntax
 vim.g.asmsyntax = "nasm"
